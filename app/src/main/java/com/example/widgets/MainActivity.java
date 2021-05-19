ef1502e72eedef45364af992ad93b1f705a317cc
b20hanwa <b20hanwa@student.his.se>
2021-05-19 18:31:14 +0200

    Added button to MainActivity.java by findViewById and imported android.widget.Button.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.MyBtn);
     }
 }
